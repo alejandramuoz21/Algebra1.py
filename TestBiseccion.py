c95e0d7779b4eb34661fd2c6af36f8661fe6c75a
Guadalupe Muñoz <52288369+alejandramuoz21@users.noreply.github.com>
2019-08-14 17:06:15 -0500

    Add files via upload

---
+++ TestBiseccion.py
@@ -0,0 +1,4 @@
+from biseccion import *
+def g(t): return -7*t**2/6 + 15*t/2 - 28/3
+s = biseccion(g, 4, 5, 0.0001)
+print(s)
---
+++ biseccion.py
@@ -0,0 +1,11 @@
+def biseccion(f, a, b, e):
+    while b -a>e:
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        else:
+            if f(a) * f(c)>0:
+                a = c
+            else:
+                b = c
+    return c
